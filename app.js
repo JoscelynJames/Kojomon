c1740e8cace612b67b46a4682c26369dc4aa487e
Nate <n8bdoesemailvia@gmail.com>
2017-09-19 13:16:46 -0600

    fixed get for pet/:id

--- app.js
+++ app.js
@@ -23,22 +23,22 @@ app.use('/api/v1/plant_type', plant_type)
 app.use('/api/v1/player', player)
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
