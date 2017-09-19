c61020f344f65b56c97eb1fde75b861808ee3401
Joscelyn James <josce.james7@hotmail.com>
2017-09-19 16:22:54 -0600

    auth0 added

--- app.js
+++ app.js
@@ -9,19 +9,73 @@ const food = require('./routes/food.js');
 const hygiene = require('./routes/hygiene.js');
 const plant_type = require('./routes/plant_type.js');
 const player = require('./routes/player.js');
-const PORT = process.env.PORT || 3000
+const auth = require('./routes/auth.js');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const assets = require('./assets.js');
+const morgan = require('morgan');
+const user = require('./routes/user.js');
+const session = require('express-session');
+// const jwt = require('express-jwt');
+// const jwks = require('jwks-rsa');
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded())
-app.use(cors())
-app.use('/api/v1/pet', pet)
-app.use('/api/v1/attention', attention)
-app.use('/api/v1/badge', badge)
-app.use('/api/v1/food', food)
-app.use('/api/v1/hygiene', hygiene)
-app.use('/api/v1/plant_type', plant_type)
-app.use('/api/v1/player', player)
+const strategy = new Auth0Strategy(
+  {
+    domain: assets.domain,
+    clientID: assets.clientID,
+    clientSecret: assets.clientSecret,
+    callbackURL: assets.callbackURL
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile);
+  }
+);
 
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+// var jwtCheck = jwt({
+//     secret: jwks.expressJwtSecret({
+//         cache: true,
+//         rateLimit: true,
+//         jwksRequestsPerMinute: 5,
+//         jwksUri: "https://kojomon.auth0.com/.well-known/jwks.json"
+//     }),
+//     audience: 'https://kojomon-userdata',
+//     issuer: "https://kojomon.auth0.com/",
+//     algorithms: ['RS256']
+// });
+
+app.use(morgan('tiny'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors());
+// app.use(jwtCheck);
+
+//routes
+app.use('/api/v1/pet', pet);
+app.use('/api/v1/attention', attention);
+app.use('/api/v1/badge', badge);
+app.use('/api/v1/food', food);
+app.use('/api/v1/hygiene', hygiene);
+app.use('/api/v1/plant_type', plant_type);
+app.use('/api/v1/player', player);
+app.use('/', auth);
+app.use('/user', user);
+
+// app.get('/authorized', function (req, res) {
+//   res.send('Secured Resource');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +92,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err.message);
+  console.error(err);
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
--- routes/player.js
+++ routes/player.js
@@ -2,6 +2,13 @@ const express = require('express');
 const knex = require('../knex/knex.js');
 const router = express.Router();
 
+router.get('/', function(req, res) {
+  return knex('player')
+    .then(function(player) {
+      res.send(player)
+    })
+})
+
 router.get('/:id', function (req, res) {
   var playerId = req.params.id
   return knex('player').where('id', playerId)
