7e71d781c080db9a7c2d88da7a0225ff65c209d3
Joscelyn James <josce.james7@hotmail.com>
2017-09-20 15:16:44 -0600

    added sprites seeds

--- migrations/20170916115603_plant_type.js
+++ migrations/20170916115603_plant_type.js
@@ -2,7 +2,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('plant_type', (table) => {
     table.increments('id').primary()
-    table.text('type')
     table.text('evo_1')
     table.text('evo_2')
     table.text('evo_3')
--- seeds/03_plant_type.js
+++ seeds/03_plant_type.js
@@ -4,8 +4,37 @@ exports.seed = function(knex, Promise) {
   return knex('plant_type').del()
     .then(function () {
       // Inserts seed entries
-      return knex('plant_type').insert([
-        {id: 1, type: "bob", evo_1: '/imgs/bob_1', evo_2: '/imgs/bob_2', evo_3: '/imgs/bob_3', evo_4: "/imgs/casket"},
+      return knex('plant_type').insert([{
+        id: 1,
+        evo_1: '/imgs/evo-path-1/evo-1.png',
+        evo_2: '/imgs/evo-path-1/evo-2.png',
+        evo_3: '/imgs/evo-path-1/evo-3.png',
+        evo_4: "/imgs/headstone.png"
+      },{
+        id: 2,
+        evo_1: '/imgs/evo-path-2/evo-1.png',
+        evo_2: '/imgs/evo-path-2/evo-2.png',
+        evo_3: '/imgs/evo-path-2/evo-3.png',
+        evo_4: "/imgs/headstone.png"
+      },{
+        id: 3,
+        evo_1: '/imgs/evo-path-3/evo-1.png',
+        evo_2: '/imgs/evo-path-3/evo-2.png',
+        evo_3: '/imgs/evo-path-3/evo-3.png',
+        evo_4: "/imgs/headstone.png"
+      },{
+        id: 4,
+        evo_1: '/imgs/evo-path-4/evo-1.png',
+        evo_2: '/imgs/evo-path-4/evo-2.png',
+        evo_3: '/imgs/evo-path-4/evo-3.png',
+        evo_4: "/imgs/headstone.png"
+      },{
+        id: 5, 
+        evo_1: '/imgs/evo-path-5/evo-1.png',
+        evo_2: '/imgs/evo-path-5/evo-2.png',
+        evo_3: '/imgs/evo-path-5/evo-3.png',
+        evo_4: "/imgs/headstone.png"
+      },
       ]);
     });
 };
