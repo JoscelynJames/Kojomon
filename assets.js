8828b6405354ee0c0213cea7f428c98447a5ddd5
Tyler Torres <tyler.john.torres@gmail.com>
2017-09-20 17:31:54 -0600

    after class testing

--- assets.js
+++ assets.js
@@ -7,7 +7,7 @@
 
 
 const assets = {
-  domain: 'kojomon.auth0.com',
+  domain: 'app77287715.auth0.com',
   clientID: '5MLBuhvrYRNgq8s29jsL4jmWFzfEDchS',
   clientSecret: 'gAVU9PvPsACmcvRuvXIc7YK1RcOg-YKvl_NEhw7wHm8V5oup-Rrael7OIsO_K3_M',
   callbackURL: 'https://gentle-peak-99253.herokuapp.com/callback'
