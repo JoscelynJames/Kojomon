e41e52492a1ab50f994a753b148c09057fb42db5
Tyler Torres <tyler.john.torres@gmail.com>
2017-09-20 10:18:56 -0600

    re deploying to heroku

--- assets.js
+++ assets.js
@@ -2,7 +2,7 @@ const assets = {
   domain: 'kojomon.auth0.com',
   clientID: 'bUL7zF8NH51FQ10GSR7dA59361gL57zk',
   clientSecret: 'qFE1ZLLvqEgtj7-8dPDq52s-i5FGVecAcp2jNGEtcnmdLTCq2WXqy6HLlh3kfTdK',
-  callbackURL: 'http://localhost:3000/callback'
+  callbackURL: 'https://gentle-peak-99253.herokuapp.com/callback'
 }
 
 module.exports = assets
