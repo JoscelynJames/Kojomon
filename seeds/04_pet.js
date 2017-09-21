b7ff99a46a7e5e7605a79464c3114894c46c6b88
Joscelyn James <josce.james7@hotmail.com>
2017-09-20 19:30:40 -0600

    added plant_type_id

--- seeds/04_pet.js
+++ seeds/04_pet.js
@@ -9,42 +9,48 @@ exports.seed = function(knex, Promise) {
         name: 'Planty',
         evo_score: 35,
         active: true,
-        creation_date: new Date()
+        creation_date: new Date(),
+        plant_type_id: 1
       }, {
         id: 2,
         player_id: 1,
         name: 'Mojo',
         evo_score: 62,
         active: true,
-        creation_date: new Date()
+        creation_date: new Date(),
+        plant_type_id: 3
       },{
         id: 3,
         player_id: 2,
         name: 'Mr.Plant',
         evo_score: 88,
         active: true,
-        creation_date: new Date()
+        creation_date: new Date(),
+        plant_type_id: 2
       },{
         id: 4,
         player_id: 3,
         name: 'Frank',
         evo_score: 20,
         active: false,
-        creation_date: new Date()
+        creation_date: new Date(),
+        plant_type_id: 4
       },{
         id: 5,
         player_id: 3,
         name: 'Plant',
         evo_score: 1,
         active: true,
-        creation_date: new Date()
+        creation_date: new Date(),
+        plant_type_id: 2
       },{
         id: 6,
         player_id: 4,
         name: 'Kojo',
         evo_score: 50,
         active: true,
-        creation_date: new Date()
+        creation_date: new Date(),
+        plant_type_id: 3
       },
 
     ]);
