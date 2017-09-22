625beea54ea1570e26c5758679ddd5b6138091e0
Joscelyn James <josce.james7@hotmail.com>
2017-09-21 18:05:35 -0600

    fixed fiel path to sprites

--- seeds/03_plant_type.js
+++ seeds/03_plant_type.js
@@ -6,34 +6,34 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('plant_type').insert([{
         id: 1,
-        evo_1: '../imgs/evo-path-1/evo-1.png',
-        evo_2: '../imgs/evo-path-1/evo-2.png',
-        evo_3: '../imgs/evo-path-1/evo-3.png',
-        evo_4: "../imgs/headstone.png"
+        evo_1: './imgs/evo-path-1/evo-1.png',
+        evo_2: './imgs/evo-path-1/evo-2.png',
+        evo_3: './imgs/evo-path-1/evo-3.png',
+        evo_4: "./imgs/headstone.png"
       },{
         id: 2,
-        evo_1: '../imgs/evo-path-2/evo-1.png',
-        evo_2: '../imgs/evo-path-2/evo-2.png',
-        evo_3: '../imgs/evo-path-2/evo-3.png',
-        evo_4: "../imgs/headstone.png"
+        evo_1: './imgs/evo-path-2/evo-1.png',
+        evo_2: './imgs/evo-path-2/evo-2.png',
+        evo_3: './imgs/evo-path-2/evo-3.png',
+        evo_4: "./imgs/headstone.png"
       },{
         id: 3,
-        evo_1: '../imgs/evo-path-3/evo-1.png',
-        evo_2: '../imgs/evo-path-3/evo-2.png',
-        evo_3: '../imgs/evo-path-3/evo-3.png',
-        evo_4: "../imgs/headstone.png"
+        evo_1: './imgs/evo-path-3/evo-1.png',
+        evo_2: './imgs/evo-path-3/evo-2.png',
+        evo_3: './imgs/evo-path-3/evo-3.png',
+        evo_4: "./imgs/headstone.png"
       },{
         id: 4,
-        evo_1: '../imgs/evo-path-4/evo-1.png',
-        evo_2: '../imgs/evo-path-4/evo-2.png',
-        evo_3: '../imgs/evo-path-4/evo-3.png',
-        evo_4: "../imgs/headstone.png"
+        evo_1: './imgs/evo-path-4/evo-1.png',
+        evo_2: './imgs/evo-path-4/evo-2.png',
+        evo_3: './imgs/evo-path-4/evo-3.png',
+        evo_4: "./imgs/headstone.png"
       },{
         id: 5,
-        evo_1: '../imgs/evo-path-5/evo-1.png',
-        evo_2: '../imgs/evo-path-5/evo-2.png',
-        evo_3: '../imgs/evo-path-5/evo-3.png',
-        evo_4: "../imgs/headstone.png"
+        evo_1: './imgs/evo-path-5/evo-1.png',
+        evo_2: './imgs/evo-path-5/evo-2.png',
+        evo_3: './imgs/evo-path-5/evo-3.png',
+        evo_4: "./imgs/headstone.png"
       },
       ]);
     });
